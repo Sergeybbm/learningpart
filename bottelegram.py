abaeb56f31fc588d9b430eef91dc3ea6d0efc850
Sergeybbm <50775296+Sergeybbm@users.noreply.github.com>
2019-12-05 18:07:03 +0500

    Bot telegram

---
+++ bottelegram.py
@@ -0,0 +1,21 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('826609662:AAEuZX9v53NkvN-HiL-tBSrly9rH10wm0Xg')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+if __name__ == '__main__':
+main()
