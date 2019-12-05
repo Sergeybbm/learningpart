ffa807b88b4d7c5168fe29646182a7c39ae56681
Sergeybbm <50775296+Sergeybbm@users.noreply.github.com>
2019-12-05 18:04:12 +0500

    Тесты изучения Питона1

---
+++ learnpy.py
@@ -0,0 +1,63 @@
+#City = input("Where are you from? ")
+#print ("Wow, Exellent From", City)
+
+#num_1 = input ( "Enter First Name: " )
+#num_2 = input ( "Enter Second Name: " )
+#num_3 = input("How old are you? ")
+
+#res = num_1 +' '+ num_2
+#res2 = num_3
+#res3 = input("Enter test: ")
+#res3 *=5 + 'Gay'
+
+#print(res3)
+#print ("Show me:" , res)
+#print ("You very young:" ,res2, "Not bad")
+
+#num_1 = input("Your Name:")
+#if num_1 == "Sergey":
+ #   print("You are Cool\n")
+ #  print("All is Ok!")
+#elif num_1 == "Victor":
+ #   print("Gay :))))")
+#else:
+#    print("Kuson")
+
+
+#i = 1
+#while i<100:
+ #   print(i)
+  #  i *=2
+
+#for j in 'Hello world':
+    #if j== 'w':
+     #   break
+    #print (j*5, end = '')
+
+#for h in 'Hello world\n':
+ #       if h == 'a':
+  #          continue
+   #     print (h*1, end ='')
+#else:
+    #print ('Буквы а нет в этих словах')
+#A=[]
+#Anton = [ 'Israel', '33',' Ashdod' ]
+#print(Anton)
+
+#i = 1000
+#while i > 100:
+#    print (i)
+ #   i /=2
+
+#for j in 'Hello World':
+   # print (j*2, end = '')
+
+age=int(input("Введите свой возраст:"))
+if age>=1 and age<3:
+     print("Вход бесплатный.")
+elif age>=3 and age<12:
+     print("Цена-100 рублей.")
+elif age>=12:
+     print("Цена-200 рублей.")
+else:
+     print("Ошибка!")
