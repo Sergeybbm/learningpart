e64bb7b099bef10071edca4352f64a957fccb2c3
Sergeybbm <50775296+Sergeybbm@users.noreply.github.com>
2019-12-05 18:23:00 +0500

    TestPy.py
    Testing Python code

---
+++ Testing.py
@@ -0,0 +1,50 @@
+#City = input("Where are you from? ")
+#print ("Wow, Exellent From", City)
+
+#num_1 = input ( "Enter First Name: " )
+#num_2 = input ( "Enter Second Name: " )
+#num_3 = input("How old are you? ")
+
+#res = num_1 +' '+ num_2
+#res2 = num_3
+#res3 = input("Enter test: ")
+#res3 *=5 + 'Gay'
+
+#print(res3)
+#print ("Show me:" , res)
+#print ("You very young:" ,res2, "Not bad")
+
+#num_1 = input("Again Your Name:")
+#if num_1 == "Sergey":
+#    print("You are Cool\n")
+#   print("All is Ok!")
+#elif num_1 == "Victor":
+#    print("Gay :))))")
+#else:
+#    print("Kusit")
+
+
+#i = 1
+#while i<100:
+    #print(i)
+    #i *=2
+
+#for j in 'Hello world':
+    #if j== 'w':
+     #   break
+    #print (j*5, end = '')
+
+#for h in 'Hello world\n':
+ #       if h == 'a':
+  #          continue
+   #     print (h*1, end ='')
+#else:
+    #print ('Буквы а нет в этих словах')
+#A=[]
+#Anton = [ 'Israel', '33',' Ashdod' ]
+#print(Anton)
+
+i = 0
+while = i < 10
+    print ('i')
+i += 2
